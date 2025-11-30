46a60ac7e3e308a67d9f5c0d069d8379a4dddc5d
Khoi <khoidang01234@gmail.com>
2025-11-30 18:35:58 +0700

    feat: add responsive styles for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -21,31 +21,43 @@ const App = () => {
   ];
 
   const testimonialsFiltered = testimonials.filter((t) => t.id === slide + 1);
+
   return (
-    <div className="relative w-screen min-h-screen font-display">
-      <img className="fixed top-40 left-60" src="pattern-quotes.svg" alt="" />
+    <div className="relative w-screen h-dvh font-display overflow-x-hidden">
+      <img
+        className="absolute w-16 sm:top-40 top-90 sm:left-60 left-30"
+        src="pattern-quotes.svg"
+        alt=""
+      />
 
-      <img className="fixed right-30 top-10 max-w-xl" src="pattern-bg.svg" alt="" />
+      <img
+        className="absolute sm:right-30 right-0 sm:top-10 top-4 sm:max-w-xl max-w-[350px]"
+        src="pattern-bg.svg"
+        alt=""
+      />
 
-      <div className="flex justify-center items-center h-screen">
+      <div className="sm:flex block mt-10 justify-center items-center h-screen">
         {testimonialsFiltered.map((t) => (
           <div
             key={t.id}
-            className="relative grid grid-cols-[3fr_1fr] w-full items-center max-w-4xl"
+            className="relative sm:grid sm:grid-cols-[3fr_1fr] flex gap-y-18 flex-col-reverse w-full items-center max-w-4xl"
           >
             <div className="z-40">
-              <p className="text-3xl max-w-xl">{t.said}</p>
-              <div className="flex gap-x-4 mt-4">
+              <p className="sm:text-3xl text-lg text-Blue-950 text-center sm:text-left sm:max-w-xl max-w-sm">
+                {t.said}
+              </p>
+              <div className="sm:flex block text-center gap-x-4 mt-4">
                 <p className="font-bold text-Blue-950">{t.name}</p>
                 <p className="font-bold text-Gray-400">{t.career}</p>
               </div>
             </div>
-            <div className="absolute right-0 shadow-lg">
-              <img className="max-w-full w-sm" src={t.image} alt="" />
+            <div className="sm:absolute right-0 shadow-lg">
+              <img className="max-w-full sm:w-sm w-3xs" src={t.image} alt="" />
             </div>
-            <div className="z-50 fixed right-110 top-120 bg-white shadow-lg flex rounded-3xl">
+
+            <div className="z-50 absolute sm:right-60 top-60 px-4 gap-x-8 sm:py-4 py-3 sm:top-70 bg-white shadow-lg flex rounded-3xl">
               <button
-                className="cursor-pointer p-4 hover:bg-Blue-950 transition-colors duration-300 rounded-l-3xl"
+                className="cursor-pointer rounded-l-3xl"
                 onClick={() =>
                   setSlide(
                     (slide - 1 + testimonials.length) % testimonials.length
@@ -55,7 +67,7 @@ const App = () => {
                 <img src="icon-prev.svg" alt="" />
               </button>
               <button
-                className="cursor-pointer p-4 hover:bg-Blue-950 transition-colors duration-300 rounded-r-3xl"
+                className="cursor-pointer rounded-r-3xl"
                 onClick={() => setSlide((slide + 1) % testimonials.length)}
               >
                 <img src="icon-next.svg" alt="" />
